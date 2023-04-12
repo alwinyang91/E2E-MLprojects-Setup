92968fcae81bae63821fe98e99b59a023a7e228c
Zewen <zewenyang91@gmail.com>
2023-04-12 17:17:43 +0200

    update

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 #os.getcwd(): get the current working directory of a process
-logs_path=os.path.join(os.getcwd(), "log", LOG_FILE)
+logs_path=os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
